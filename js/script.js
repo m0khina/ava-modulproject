ad52c8033a68d81a3d30c30a627b94737835994d
m0khina <mohina.artikova.kk@gmail.com>
2022-12-01 19:06:05 +0300

    great project

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    centeredSlides: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
